a1df5b6dbc72bd694bd696447a96984daab69622
abdulmalik <abdalmalik2015@gmail.com>
2021-06-03 17:12:33 +0300

    initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    temp: "F:\\temp",
+    upload: "F:\\upload",
+    dbUrl: "mongodb://localhost/mohammedBlog",
+    mailKey: "e274eb0c415f6d26e528a8ac0b93580f-19f318b0-73d95b53",
+    mailDomain: "abdulmalik.fikracamp.com",
+    mailTo: "",
+    mailFrom: "",
+    domain: "localhost:3000",
+
+  },
+};
